862cba0f1f85a8257638abdfcff3e0401d8b39d1
guilherme <victorguilherme015@gmail.com>
2025-05-29 16:13:08 -0300

    feat: module backend

--- dominio-prestacaoServicos/src/main/java/com/exemple/implementacaoweb2/avaliacao/TipoAvaliacao.java
+++
@@ -1,6 +0,0 @@
-package com.exemple.implementacaoweb2.avaliacao;
-
-public enum TipoAvaliacao {
-    CLIENTE_AVALIA_PRESTADOR,
-    PRESTADOR_AVALIA_CLIENTE
-}
--- dominio-prestacaoServicos/src/main/java/com/exemple/implementacaoweb2/pedidos/StatusPedido.java
+++
@@ -1,7 +0,0 @@
-package com.exemple.implementacaoweb2.pedidos;
-
-public enum StatusPedido {
-    PENDENTE,
-    CONCLUIDO,
-    CANCELADO;
-}
