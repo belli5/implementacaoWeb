065c54821a9afa52bf4e3c5b746b04cf20815441
GabrielBza <Gabrielbzandrade@gmail.com>
2025-06-01 20:17:54 -0300

    Código Unificado

--- backend/src/main/java/com/exemple/backend/apresentacao/controllers/AvaliacaoSobrePrestadorController.java
+++ backend/src/main/java/com/exemple/backend/apresentacao/controllers/AvaliacaoSobrePrestadorController.java
@@ -1,4 +1,14 @@
 package com.exemple.backend.apresentacao.controllers;
 
+
+import com.exemple.backend.dominio.services.AvaliacaoSobrePrestadorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/avaliacaoSobrePrestador")
 public class AvaliacaoSobrePrestadorController {
+    @Autowired
+    private AvaliacaoSobrePrestadorService avaliacaoSobrePrestadorService;
 }
